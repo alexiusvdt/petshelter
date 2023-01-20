b48ee9b84608e1983c02f3194ee3164f3827d175
Alex Johnson <alex.johnson293@gmail.com>
2023-01-20 10:00:59 -0800

    add migration and controller

---
+++ PetShelterApi/Controllers/AnimalsController.cs
@@ -0,0 +1,119 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetShelterApi.Models;
+
+namespace PetShelterApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private readonly PetShelterApiContext _db;
+    
+    public AnimalsController(PetShelterApiContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet ]
+    public async Task<ActionResult<IEnumerable<Animal>>> Get(string species, string name, int minimumAge)
+    {
+      IQueryable<Animal> query = _db.Animals.AsQueryable();
+
+      if (species != null)
+      {
+        query = query.Where(entry => entry.Species == species);
+      }
+      if (species != null && subspecies != null)
+      {
+        query = query.Where(entry => entry.SubSpecies == subspecies);
+      }
+      
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+      if (minimumAge > 0)
+      {
+        query = query.Where(entry => entry.Age >= minimumAge);
+      }
+
+      return await query.ToListAsync();
+    }
+
+
+    // GET api/Animals/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+      Animal animal = await _db.Animals.FindAsync(id);
+
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      return animal;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Animal>> Post(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetAnimal), new {id = animal.AnimalId }, animal);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal)
+    {
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+
+      _db.Animals.Update(animal) ;
+      
+      //async saving
+      try 
+      {
+      await _db.SaveChangesAsync();
+      }
+      
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+    return NoContent();
+
+    }
+
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id)
+    {
+      Animal animal = await _db.Animals.FindAsync(id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+  }
+}
---
+++ PetShelterApi/Migrations/20230120170209_Initial.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetShelterApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Species = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SubSpecies = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    ShelterDate = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimalId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Name", "ShelterDate", "Species", "SubSpecies" },
+                values: new object[,]
+                {
+                    { 1, 7, "Denna", "01/20/2023", "Cat", "Himalayan" },
+                    { 2, 2, "Kvothe", "12/20/2022", "Dog", "Borzoi" },
+                    { 3, 5, "Haliax", "01/03/2023", "Dog", "Poodle" },
+                    { 4, 5, "Bast", "01/10/2023", "Cat", "Domestic Shorthair" },
+                    { 5, 10, "OreSeur", "12/15/2023", "Dog", "Wolfhound" },
+                    { 6, 4, "Elend", "01/10/2023", "Dog", "Jack Russel Terrier" },
+                    { 7, 3, "Kjelvin", "01/18/2023", "Cat", "Abyssinian" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
