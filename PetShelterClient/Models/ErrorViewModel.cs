60cd74176b8b729a87364f40ae2a4923548a21d7
Alex Johnson <alex.johnson293@gmail.com>
2023-01-22 13:32:24 -0800

    remove frontend from project - too many display bugs

--- PetShelterClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PetShelterClient.Models;
-
-#nullable enable
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
